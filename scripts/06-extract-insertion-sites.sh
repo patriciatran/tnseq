cfd62ea670759c29d59fdb34bfe5bbc77f00da77
Patricia Tran <ptran5@wisc.edu>
2025-07-17 15:54:20 -0500

    using a python script instead of awk

--- scripts/06-extract-insertion-sites.sh
+++ scripts/06-extract-insertion-sites.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+#arguments = $(sample) $(ref) $(staging) $(request_cpus)
 
 SAMPLE="$1"
+REF="$2"
+STAGING="$3"
+CPUS="$4"
 
-bedtools bamtobed -i ${SAMPLE}_sorted.bam | awk 'BEGIN{OFS="\t"} {if ($6=="+") print $1, $2, $2+1; else print $1, $3-1, $3}' > ${SAMPLE}_insertions.bed
+ls -lht
+
+bedtools bamtobed -i ${STAGING}/aligned/${SAMPLE}_sorted.bam > bamtobed_output.bed
+
+ls -lht
+
+python extract_insertions.py bamtobed_output.bed
+
+# this creates a file insertions.bed
+
+ls -lht
